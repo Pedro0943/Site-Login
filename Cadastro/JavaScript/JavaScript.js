ca5d945c30f84630c18bfe24f3b7017c457975c9
Pedro Henrique <129074294+Pedro0943@users.noreply.github.com>
2024-12-29 13:30:04 -0300

    Update JavaScript.js

--- Cadastro/JavaScript/JavaScript.js
+++ Cadastro/JavaScript/JavaScript.js
@@ -1,3 +1,22 @@
+//desabilita o botão no início 
+document.getElementById("submit").disabled = true;
+
+//cria um event listener que escuta mudanças no input
+document.getElementById("nome").addEventListener("input", function(event){
+      
+    //busca conteúdo do input
+    var conteudo = document.getElementById("nome").value;
+    
+    //valida conteudo do input 
+    if (conteudo !== null && conteudo !== '') {
+    //habilita o botão
+    document.getElementById("submit").disabled = false;
+    } else {
+    //desabilita o botão se o conteúdo do input ficar em branco
+    document.getElementById("submit").disabled = true;
+    }
+});
+
 function mostrarOuOcultarSenha() {
     var senha = document.getElementById("senha");
     var checkbox = document.getElementById("mostrarSenha");
